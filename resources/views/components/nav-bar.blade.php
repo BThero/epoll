53c9ff42dc7678b4d55a9a1dc8d053ed56ef9697
Temirlan Baibolov <bthero09@gmail.com>
2023-11-23 22:54:05 +0100

    fix: navbar

--- resources/views/components/nav-bar.blade.php
+++ resources/views/components/nav-bar.blade.php
@@ -1,27 +1,33 @@
 <nav class="not-format bg-white">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/home" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Epoll</span>
+        <a href="/home" class="flex items-center rtl:space-x-reverse">
+            <span
+                class="self-center text-3xl font-semibold text-gray-700 whitespace-nowrap dark:text-white">Epoll</span>
         </a>
         <div class="hidden w-full md:block md:w-auto" id="navbar-default">
             <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
-                    <x-navlink href="home">
+                    <x-navlink href="/home">
                         Home
                     </x-navlink>
                 </li>
                 <li>
-                    <x-navlink href="polls">
+                    <x-navlink href="/polls">
                         Polls
                     </x-navlink>
                 </li>
                 <li>
-                    <x-navlink href="responses">
+                    <x-navlink href="/responses">
                         Responses
                     </x-navlink>
                 </li>
                 <li>
-                    <x-navlink href="profile">
+                    <x-navlink href="/options">
+                        Options
+                    </x-navlink>
+                </li>
+                <li>
+                    <x-navlink href="/profile">
                         Profile
                     </x-navlink>
                 </li>
