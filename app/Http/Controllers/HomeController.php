9f2b9999348122ba0e8f06b09ecafb04920b9eb9
Temirlan Baibolov <bthero09@gmail.com>
2023-11-24 03:58:54 +0100

    feat: show count of new responses

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -12,10 +13,15 @@ class HomeController extends Controller
     public function __invoke(Request $request)
     {
         $user = $request->user();
-        $polls = $user->polls()->get();
+        $new_responses = DB::table('responses')
+            ->where('responses.created_at', '>', now()->subDays(1))
+            ->join('polls', 'responses.poll_id', '=', 'polls.id')
+            ->where('polls.user_id', '=', $user->id)
+            ->select('responses.id')
+            ->count();
 
         return view('home', [
-            'polls' => $polls,
+            'new_responses' => $new_responses,
         ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,5 +8,13 @@
         @else
             <p class="text-end">Welcome stranger! Please fill out your name in Profile page</p>
         @endisset
+
+        @if ($new_responses === 0)
+            <p class="text-end">There were no responses within the last day.</p>
+        @elseif ($new_responses === 1)
+            <p class="text-end">There is 1 new response within the last day.</p>
+        @else
+            <p class="text-end">There are {{ $new_responses }} new responses within the last day.</p>
+        @endif
     </main>
 </x-signed-in-layout>
